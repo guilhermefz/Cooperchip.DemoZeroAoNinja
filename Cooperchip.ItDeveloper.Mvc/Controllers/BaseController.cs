9591e704404dfa968abb62f9593bc952e5ca22ca
Guilherme.Campos <Guilherme.Campos@DKTRAINE07>
2025-08-27 11:44:11 -0300

    Add View Paciente

---
+++ Cooperchip.ItDeveloper.Mvc/Controllers/BaseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cooperchip.ItDeveloper.Mvc.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+            
+    }
+}
---
+++ Cooperchip.ItDeveloper.Mvc/Controllers/PacienteController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cooperchip.ItDeveloper.Mvc.Controllers
+{
+    [Route("")]
+    [Route("gestao-de-pacientes")]
+    public class PacienteController : BaseController
+    {
+        [Route("pacientes")]
+        //[HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AdicionarPaciente()
+        {
+            return View();
+        }
+
+    }
+}
