f69cb475ba9f04c00cc656f1b896b87783209093
Guilherme.Campos <Guilherme.Campos@DKTRAINE07>
2025-08-28 14:46:04 -0300

    full dataAnnotations

--- Cooperchip.ItDeveloper.Mvc/Models/EstadoPacienteViewModel.cs
+++ Cooperchip.ItDeveloper.Mvc/Models/EstadoPacienteViewModel.cs
@@ -1,10 +1,15 @@
 ﻿using Cooperchip.ItDeveloper.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace Cooperchip.ItDeveloper.Mvc.Models
 {
     public class EstadoPacienteViewModel
     {
+        [Key]
         public Guid Id { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatório!")]
+        [Display(Name = "Descrição")]
+        [StringLength(20, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres", MinimumLength = 3)]
         public string? Descricao { get; set; }
         public virtual ICollection<Paciente>? Pacientes { get; set; }
     }
