00ac0029e6a5eebb4aa70b7131c2f913b2160ca4
Guilherme Fiuza <guilherme_f_campos@estudante.sesisenai.org.br>
2025-09-22 16:49:08 -0300

    Code reoganization

---
+++ Cooperchip.ItDeveloper.Mvc/Extensions/ExtensionsMethods/DataExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace Cooperchip.ItDeveloper.Mvc.Extensions.ExtensionsMethods
+{
+    public static class DataExtension
+    {
+
+        public static string ToBrazilianDate(this DateTime valor)
+        {
+            return valor.ToString("dd/MM/yyyy");
+        }
+
+        public static string ToBrazilianDateTime(this DateTime valor)
+        {
+            return valor.ToString("dd/MM/yyyy HH:mm:ss");
+        }
+    }
+}
